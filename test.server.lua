81b4ff6cddf68b3ee42cca37d1239e49aace7852
Quamatic <lman71037@gmail.com>
2023-04-21 19:14:13 -0500

    Add comment

--- test.server.lua
+++ test.server.lua
@@ -1,3 +1,4 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
+print("Starting Redux tests... (This may take a while)")
 require(ReplicatedStorage.TestEZ).TestBootstrap:run({ ReplicatedStorage.Tests })
